e534e606f31f6ec3470537ee2d2f2b457f0074a7
Steven Thomas <st01634@surrey.ac.uk>
2024-02-04 01:59:10 +0000

    made some new folders

--- UltraShorts/settings.py
+++ UltraShorts/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
