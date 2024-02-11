a8f63caaee02b7f8bc1d010a70d8d4a6ea326f0d
Steven Thomas <st01634@surrey.ac.uk>
2024-02-11 01:26:06 +0000

    Added the buttons finally Need to now implement the copy to clipboard function

---
+++ static/js/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        console.log('Copying to clipboard was successful!');
+    }, function(err) {
+        console.error('Could not copy text: ', err);
+    });
+}
