46ce0f8ea767f15fb1bc9ef29f0da72d3deddaba
Steven Thomas <st01634@surrey.ac.uk>
2024-02-11 14:41:02 +0000

    CHUNKING IS FINISHED

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,27 @@
 function copyToClipboard(chunkText) {
-    // Decode Unicode escape sequences
+
     const decodedText = decodeURIComponent(JSON.parse('"' + chunkText.replace(/\"/g, '\\"') + '"'));
-
     navigator.clipboard.writeText(decodedText).then(function() {
-        console.log('Copying to clipboard was successful!');
 
-        // Show copy confirmation
+        // Get the confirmation alert element
         const confirmation = document.getElementById('copyConfirmation');
+        
+        // Make sure the element is visible and fully opaque before starting the fade out
         confirmation.style.display = 'block';
+        confirmation.style.opacity = '1';
+        
+        // Hide the confirmation message after 1 second with a fade out
         setTimeout(() => {
-            confirmation.style.display = 'none';
-        }, 1000); // Display the message for 1 second
+            // Start the fade out by reducing the opacity
+            confirmation.style.opacity = '0';
+
+            // After the fade out duration, set display to none
+            setTimeout(() => {
+                confirmation.style.display = 'none';
+            }, 500); // Set this to match your CSS transition duration
+        }, 2000); // Keep the message visible for 1 second before starting the fade out
     }, function(err) {
-        console.error('Could not copy text:', err);
+        console.error('Could not copy text: ', err);
     });
 }
+
