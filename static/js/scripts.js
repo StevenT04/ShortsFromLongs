78ffc3754df8a7c26792f5163e71e9e5bf880e85
Steven Thomas <st01634@surrey.ac.uk>
2024-02-11 01:43:41 +0000

    Removed not working JS Stuff Need to add copy to ClipBoard functionality

--- static/js/scripts.js
+++
@@ -1,8 +0,0 @@
-// scripts.js
-function copyToClipboard(text) {
-    navigator.clipboard.writeText(text).then(function() {
-        console.log('Copying to clipboard was successful!');
-    }, function(err) {
-        console.error('Could not copy text: ', err);
-    });
-}
