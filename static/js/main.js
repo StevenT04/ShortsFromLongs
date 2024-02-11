d1c960ec3fd1af2a8dc57f1a263415a937ffd16d
Steven Thomas <st01634@surrey.ac.uk>
2024-02-11 13:32:58 +0000

    failed js

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // Hide the toast initially
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'));
+    var toastList = toastElList.map(function(toastEl) {
+        return new bootstrap.Toast(toastEl); // Initialize Bootstrap toasts
+    });
+
+    // Example: to hide a toast, you'd now use toastList[0].hide() assuming you have at least one toast.
+});
+
 function copyToClipboard(chunkText) {
     navigator.clipboard.writeText(chunkText).then(function() {
         console.log('Copying to clipboard was successful!');
-
-        // Show copy confirmation
-        const confirmation = document.getElementById('copyConfirmation');
-        confirmation.style.display = 'block';
-        setTimeout(() => {
-            confirmation.style.display = 'none';
-        }, 1000); // Display the message for 1 second
+        // Show Bootstrap toast for copy confirmation without jQuery
+        var toastElement = document.getElementById('copyConfirmation');
+        var toast = new bootstrap.Toast(toastElement);
+        toast.show();
     }, function(err) {
         console.error('Could not copy text:', err);
     });
