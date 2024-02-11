37bb5b99c14152c744cf18225d8432dfa50c3fc2
Steven Thomas <st01634@surrey.ac.uk>
2024-02-11 15:53:51 +0000

    Fixed URI Decoding Errors so all Chunks work need to fix templates

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,14 @@
 function copyToClipboard(chunkText) {
+    // Unescape Unicode escape sequences
+    const unescapedText = chunkText.replace(/\\u([\dA-F]{4})/gi, (match, grp) => {
+        return String.fromCharCode(parseInt(grp, 16));
+    }).replace(/\\n/g, "\n") // Unescape new lines
+      .replace(/\\'/g, "'") // Unescape single quotes
+      .replace(/\\"/g, '"') // Unescape double quotes
+      .replace(/\\\\/g, '\\'); // Unescape backslashes
 
-    const decodedText = decodeURIComponent(JSON.parse('"' + chunkText.replace(/\"/g, '\\"') + '"'));
-    navigator.clipboard.writeText(decodedText).then(function() {
+    navigator.clipboard.writeText(unescapedText).then(function() {
+        console.log('Copying to clipboard was successful!');
 
         // Get the confirmation alert element
         const confirmation = document.getElementById('copyConfirmation');
@@ -19,9 +26,8 @@ function copyToClipboard(chunkText) {
             setTimeout(() => {
                 confirmation.style.display = 'none';
             }, 500); // Set this to match your CSS transition duration
-        }, 2000); // Keep the message visible for 1 second before starting the fade out
+        }, 2000); // Keep the message visible for 2 seconds before starting the fade out
     }, function(err) {
         console.error('Could not copy text: ', err);
     });
 }
-
