1213e36765a8994d44fcf3348e85afa8023e1d70
StevenT04 <steventhomas1504@gmail.com>
2024-02-22 12:40:19 +0000

    Works nicely if I download videos seperately

--- trans_parse_process/views.py
+++ trans_parse_process/views.py
@@ -15,12 +15,16 @@ import time
 import requests
 from django import forms
 import os
-from .forms import ChunkingForm, ProcessingForm 
+from .forms import ChunkingForm, ProcessingForm
 from django.db.utils import IntegrityError
 from django.db.models import Q
 import ffmpeg
 import cv2
+import logging
 
+# Configure logging at the beginning of your views.py file or before the function definition
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('pytube')  # Specifically capture logs from pytube
 
 
 # Create your views here.
@@ -39,12 +43,12 @@ def show_video_list(request):
         videos_in_topic = Video.objects.filter(topic=topic_code)
         if videos_in_topic.exists():
             topic_videos.append((topic_name, videos_in_topic))
-    
+
     return render(request, 'video_list.html', {'topic_videos': topic_videos})
 
 def delete_video(request, video_id=None, video_slug=None):
     video = get_object_or_404(Video, pk=video_id)
-    
+
     # Build the path for the video file and thumbnail
     video_file_path = os.path.join(settings.MEDIA_ROOT, str(video.video_file))
     thumbnail_path = os.path.join(settings.MEDIA_ROOT, str(video.thumbnail))
@@ -59,7 +63,7 @@ def delete_video(request, video_id=None, video_slug=None):
     # Now, delete the video object
     video.delete()
     messages.add_message(request, messages.INFO, _('Video Deleted'))
-    
+
     return HttpResponseRedirect(reverse_lazy('page_video_list'))
 
 def confirm_delete_video(request, video_id=None, video_slug=None):
@@ -79,28 +83,33 @@ def process_form(request):
             url = form.cleaned_data['youtube_url']
             title = form.cleaned_data['youtube_title']
             topic = form.cleaned_data['youtube_topic']
- 
+
+            logger.debug(f"Processing form for URL: {url}")  # Example of adding your own log statement
+
             # Check if the video already exists in the database
             if Video.objects.filter(url=url).exists():
                 messages.error(request, 'This video has already been downloaded.')
                 return render(request, 'home.html', {'form': form})
- 
+
             yt = YouTube(url)
-            stream = yt.streams.filter(file_extension='mp4').get_highest_resolution()
+            logger.debug("YouTube object created")  # Log after creating YouTube object
+            # Assuming itag 22 is the one you want for 720p resolution
+            stream = yt.streams.get_lowest_resolution()
             if stream:
                 # Generate the filename
+                logger.debug(f"Stream found: {stream}")  # Log the stream that was found
                 filename = title + '.mp4'
                 save_path = os.path.join(settings.MEDIA_ROOT, 'videos')
                 file_path = os.path.join(save_path, filename)
- 
+
                 # Check if the file already exists
                 if os.path.exists(file_path):
                     messages.error(request, 'This video has already been downloaded.')
                     return render(request, 'home.html', {'form': form})
- 
+
                 # Download video
                 stream.download(output_path=save_path, filename=filename)
- 
+
                 # Download thumbnail
                 thumbnail_url = yt.thumbnail_url
                 response = requests.get(thumbnail_url)
@@ -114,7 +123,7 @@ def process_form(request):
                     # Use default thumbnail if download fails
                     thumbnail_file = 'default_image.png'
                     messages.warning(request, 'Failed to download thumbnail, using default.')
- 
+
                 # Create a new Video instance and save it to the database
                 video = Video(
                     url=url,
@@ -132,7 +141,7 @@ def process_form(request):
                     if thumbnail_file != 'default_image.png':
                         os.remove(thumbnail_path)
                     return render(request, 'home.html', {'form': form})
- 
+
                 # Redirect to the video list page
                 messages.success(request, 'Video downloaded successfully.')
                 return HttpResponseRedirect('/video-list/')
@@ -145,7 +154,7 @@ def process_form(request):
     else:
         # If not POST, redirect to home page
         return redirect('page_home')
-    
+
 def video_detail(request, video_id):
     video = get_object_or_404(Video, pk=video_id)
     chunks = video.chunks.all()  # Assuming a related_name='chunks' in the Video model
@@ -159,7 +168,7 @@ def chunk_video(request, video_id):
     if video.chunks.exists():
         video.chunks.all().delete()
         messages.info(request, "Existing Chunks Deleted, Re-Chunked now.")
-        
+
 
     try:
         video_id = parse_qs(urlparse(video.url).query).get('v', [None])[0]
@@ -167,14 +176,14 @@ def chunk_video(request, video_id):
             raise Exception("Invalid YouTube URL.")
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
         chunks = get_and_split_transcript(transcript, video.title, 17000)
-        
+
         # Save chunks to database
         for chunk_text in chunks:
             Chunk.objects.create(video=video, text=chunk_text)
         messages.success(request, "Video transcript chunked successfully.")
     except Exception as e:
         messages.error(request, f"Failed to chunk transcript: {e}")
-    
+
     return redirect('video_detail', video_id=video.id)
 
 # ...
@@ -198,14 +207,14 @@ def get_and_split_transcript(transcript, video_title, chunk_size):
         f"[VIDEO TITLE]: {video_title}\n\n"
         "[VIDEO TRANSCRIPT CHUNK]:\n\n"
     )
-    
+
     chunks, current_chunk, current_chunk_size = [], "", 0
 
     for i in range(0, len(transcript), 5):  # Process every 5th entry
         texts = ' '.join(transcript[j]['text'] for j in range(i, min(i+5, len(transcript))))
         timestamp = time.strftime('%H:%M:%S' if transcript[i]['start'] >= 3600 else '%M:%S', time.gmtime(transcript[i]['start']))
         entry = f"[{timestamp}] {texts}\n"
-        
+
         # Initialize the first chunk with prompt + first entry or append entry to current chunk
         if not chunks and not current_chunk:  # Initialize the first chunk with prompt
             current_chunk = prompt_template
@@ -295,11 +304,11 @@ def confirm_delete_templates(request, video_id):
 
 def delete_templates(request, video_id):
     video = get_object_or_404(Video, pk=video_id)
-    
+
     # Assuming 'clips' is the related name for clip templates associated with the video
     video.clips.all().delete()
     messages.add_message(request, messages.INFO, _('Template Clips Deleted'))
-    
+
     # Use 'reverse' for dynamic URL resolution
     return HttpResponseRedirect(reverse_lazy('page_processing', kwargs={'video_id': video.id}))
 
@@ -363,38 +372,41 @@ def detect_face_and_clip_with_aspect_ratio(input_file, output_file, start_time, 
         audio_stream = vid_stream.audio
 
         # Crop video and apply filters for enhancement and vignette effect
-        filtered_vid_stream = vid_stream.filter('crop', w=max_crop_width, h=max_crop_height, x=crop_x, y=crop_y)
+        filtered_vid_stream = vid_stream.filter('crop', w=max_crop_width, h=max_crop_height, x=crop_x, y=crop_y) \
+                                                .filter('vignette') \
+                                                .drawtext(text='VALUEWORK', fontfile='/Users/stevenmain/Downloads/Anurati-Regular.otf', fontsize=40, fontcolor='white', x='(main_w-text_w)/2', y='(main_h-text_h)/2')
+
         # Output - combine enhanced video with original audio
-        ffmpeg.output(filtered_vid_stream, audio_stream, output_file, acodec='copy', vcodec='libx264', preset='veryslow', crf=5).run()
+        ffmpeg.output(filtered_vid_stream, audio_stream, output_file, acodec='copy', vcodec='libx264', preset='medium', crf=17).run()
     else:
         # If no face was detected, clip the video segment without cropping.
         ffmpeg.input(input_file, ss=start_time, t=duration) \
               .output(output_file, acodec='copy', vcodec='libx264', preset='veryslow', crf=5).run()
-              
-              
+
+
 def download_clip(request, clip_id):
     clip = get_object_or_404(ShortClip, pk=clip_id)
-    
+
     if clip.clip_file:
         # Clip already exists, redirect or serve the existing file
         # Redirect back to manage clips page
         messages.error(request, "Clip already downloaded.")
         return redirect('manage_clips', video_id=clip.video_id)
-    
+
     input_file = os.path.join(settings.MEDIA_ROOT, str(clip.video.video_file))
     output_file = os.path.join(settings.MEDIA_ROOT, "clips", f"clip_{clip.pk}.mp4")
-    
+
     # Convert DurationField to total seconds
     start_time = clip.start_time.total_seconds()
     duration = (clip.end_time - clip.start_time).total_seconds()
-    
+
     # Call the adapted function
     detect_face_and_clip_with_aspect_ratio(input_file, output_file, start_time, duration)
-    
+
     # After processing, update the clip_file field
     clip.clip_file = os.path.join("clips", f"clip_{clip.pk}.mp4")
     clip.save()
-    
+
     # Redirect back to manage clips page or directly serve the file
     return redirect('manage_clips', video_id=clip.video_id)
     # If you want to directly serve the file, uncomment the following lines:
@@ -402,8 +414,8 @@ def download_clip(request, clip_id):
     #     response = HttpResponse(fh.read(), content_type="video/mp4")
     #     response['Content-Disposition'] = f'attachment; filename=clip_{clip.pk}.mp4'
     #     return response
-    
-    
+
+
 def confirm_delete_clip(request, clip_id=None):
     clip = get_object_or_404(ShortClip, pk=clip_id)
     if request.method == 'POST':
@@ -412,21 +424,21 @@ def confirm_delete_clip(request, clip_id=None):
     else:
         # Show confirmation page
         return render(request, 'confirm_delete_clip.html', {'clip': clip})
-    
+
 def delete_clip(request, clip_id=None):
     clip = get_object_or_404(ShortClip, pk=clip_id)
     video_id = clip.video.id  # Capture the video_id before deleting the clip
-    
+
     clip_file_path = os.path.join(settings.MEDIA_ROOT, str(clip.clip_file))
-    
+
     # Check if the clip file exists and delete it
     if clip.clip_file and os.path.isfile(clip_file_path):
         os.remove(clip_file_path)
-    
+
     # Delete the clip object
     clip.delete()
     messages.add_message(request, messages.INFO, 'Clip Deleted')
-    
+
     # Correctly use the reverse function to generate the URL for redirection
     manage_clips_url = reverse('manage_clips', kwargs={'video_id': video_id})
     return HttpResponseRedirect(manage_clips_url)
@@ -435,16 +447,16 @@ def delete_clip(request, clip_id=None):
 def delete_clip_download(request, clip_id=None):
     clip = get_object_or_404(ShortClip, pk=clip_id)
     video_id = clip.video.id  # Capture the video_id before deleting the clip
-    
+
     clip_file_path = os.path.join(settings.MEDIA_ROOT, str(clip.clip_file))
 
     # Check if the clip file exists and delete it
     if clip.clip_file:
         clip.clip_file.delete()
-        
+
     if os.path.isfile(clip_file_path):
         os.remove(clip_file_path)
-        
+
     messages.add_message(request, messages.INFO, 'Clip Download Deleted')
 
     # Correctly use the reverse function to generate the URL for redirection
